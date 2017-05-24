9515fbabaff963b23dfa94d5bde039263842671a
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-05-24 11:51:39 -0700

    Adds film and actor

--- app/Actor.php
+++ app/Actor.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Actor extends Model
 {
+    use Searchable;
     protected $table = 'actor';
     protected $primaryKey = 'actor_id';
     public $timestamps = false;
--- app/Film.php
+++ app/Film.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Film extends Model
 {
+    use Searchable;
     protected $table = 'film';
     protected $primaryKey = 'film_id';
     public $timestamps = false;
