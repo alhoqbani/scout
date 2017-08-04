b6de39a8d0874234f95ecf825635687f3d007231
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-08-04 04:25:40 -0700

    add components

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2">
-
+                <vue-app></vue-app>
             </div>
         </div>
     </div>
